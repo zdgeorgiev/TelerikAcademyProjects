d615db557a82562168a7bb33cff0c801fcabf8be
zdravko <zddavko@gmail.com>
2013-02-22 17:23:32 +0200

    fixed

---
+++ Programming/OOP/03. Emuns/Battery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public enum BatteryType
+{
+    LiIon, NiMH, NiCd
+}
+
+class Battery
+{
+    private string batteryModel;
+    private int? hoursIdle;
+    private int? hoursTalk;
+
+    public Battery() 
+        : this(null, null, null)
+    {
+    }
+
+    public Battery(string batteryModel)
+        : this(null, null, batteryModel)
+    {
+    }
+
+    public Battery(int? hoursIdle, int? hoursTalk)
+        : this(hoursIdle, hoursTalk, null)
+    {
+    }
+
+    public Battery(int? hoursIdle, int? hoursTalk, string batteryModel)
+    {
+        this.batteryModel = batteryModel;
+        this.hoursIdle = hoursIdle;
+        this.hoursTalk = hoursTalk;
+    }
+
+    public string BatteryModel
+    {
+        get { return this.batteryModel; }
+        set { this.batteryModel = value; }
+    }
+
+    public int? HoursIdle
+    {
+        get { return this.hoursIdle; }
+        set { this.hoursIdle = value; }
+    }
+
+    public int? HoursTalk
+    {
+        get { return this.hoursTalk; }
+        set { this.hoursTalk = value; }
+    }
+}
---
+++ Programming/OOP/03. Emuns/GSM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class GSM
+{
+    public Display Display = new Display(null, null);
+    public Battery Battery = new Battery(null, null, null);
+
+    private string model;
+    private decimal? price;
+    private string owner;
+    private string manufacturer;
+
+    public GSM(string model, string manufacturer)
+        : this(model, manufacturer, null, null, null, null)
+    {
+    }
+
+    public GSM(string model, string manufacturer, string owner, decimal? price)
+        : this(model, manufacturer, owner, price, null, null)
+    {
+    }
+
+    public GSM(string model, string manufacturer, string owner)
+        : this(model, manufacturer, owner, null, null, null)
+    {
+    }
+
+    public GSM(string model, string manufacturer, string owner, decimal? price, Display display, Battery battery)
+    {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.owner = owner;
+        this.price = price;
+        this.Battery = battery;
+        this.Display = display;
+    }
+
+    public string Model
+    {
+        get { return this.model; }
+        set { this.model = value; }
+    }
+
+    public decimal? Price
+    {
+        get { return this.price; }
+        set { this.price = value; }
+    }
+
+    public string Owner
+    {
+        get { return this.owner; }
+        set { this.owner = value; }
+    }
+
+    public string Manufacturer
+    {
+        get { return this.manufacturer; }
+        set { this.manufacturer = value; }
+    }
+}
