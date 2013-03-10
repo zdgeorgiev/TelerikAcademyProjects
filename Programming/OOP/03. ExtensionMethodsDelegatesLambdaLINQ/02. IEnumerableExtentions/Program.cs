3c839dbd2158bb21103523d52416a65e74fa3843
zdravko <zddavko@gmail.com>
2013-03-10 23:38:35 +0200

    fixed

---
+++ Programming/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/02. IEnumerableExtentions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[15];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i;
+        }
+        numbers[0] = 1;
+
+        Console.WriteLine("Average - {0}",numbers.Average()); //7.06666666666..
+        Console.WriteLine("Min value - {0}", numbers.Min()); //1
+        Console.WriteLine("Max value - {0}", numbers.Max()); //14
+        Console.WriteLine("Product - {0}", numbers.Product()); //Some big Number
+    }
+}
