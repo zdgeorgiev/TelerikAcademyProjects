b1d1c1c26454966fae7d529dbc5b3e9b6b9b1c18
zdravko <zddavko@gmail.com>
2013-03-16 20:37:17 +0200

    fixed

---
+++ Programming/OOP/05. FundamentalPrinciplesII/01. Shapes/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Circle : Shape
+{
+    public Circle(double radios)
+        : base(radios, radios) //width = radios, height = radios
+    {
+    }
+
+    public override double CalcSurface()
+    {
+        return Math.PI * this.Width * this.Width; //because width is equal to radios
+    }
+}
---
+++ Programming/OOP/05. FundamentalPrinciplesII/01. Shapes/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+abstract class Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Shape(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public abstract double CalcSurface();
+}
