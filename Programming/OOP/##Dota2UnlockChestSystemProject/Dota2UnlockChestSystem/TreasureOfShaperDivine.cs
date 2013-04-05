cff7ae62c5d83ab78ec514795d70ce6d5da8bec8
zdravko <zddavko@gmail.com>
2013-04-05 19:54:35 +0300

    fixed

--- Programming/OOP/##Dota2UnlockChestSystemProject/Dota2UnlockChestSystem/TreasureOfShaperDivine.cs
+++ Programming/OOP/##Dota2UnlockChestSystemProject/Dota2UnlockChestSystem/TreasureOfShaperDivine.cs
@@ -20,7 +20,21 @@ public class TreasureOfShaperDivine : Chest
             new Item(ReleasedItem.Ancient_Crescent, global::ItemRarity.Rare),
             new Item(ReleasedItem.The_Horned_Mother, global::ItemRarity.Mythical),
             new Item(ReleasedItem.Feral_the_Werebat, global::ItemRarity.Mythical),
-            new Item(ReleasedItem.Wings_of_the_Ruby_Web, global::ItemRarity.Legendary)
+            new Item(ReleasedItem.Wings_of_the_Ruby_Web, global::ItemRarity.Legendary),
+
+            new Item(ReleasedItem.Fearless_Badger, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Stumpy_the_Nature_Attendant, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Trusty_Mountain_Yak, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Enduring_War_Dog, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Mighty_Boar, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Morok_the_Mechanical_Mediary, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Skip_the_Delivery_Frog, global::ItemRarity.Legendary),
+            new Item(ReleasedItem.Speed_Demon, global::ItemRarity.Legendary),
+            new Item(ReleasedItem.Tickled_Tegu, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.Baby_Roshan, global::ItemRarity.Legendary),
+            new Item(ReleasedItem.Prismatic_Drake, global::ItemRarity.Mythical),
+            new Item(ReleasedItem.NaVi_Weaselcrow, global::ItemRarity.Legendary),
+            new Item(ReleasedItem.Cluckles_the_Brave, global::ItemRarity.Legendary),
         };
     }
 }
