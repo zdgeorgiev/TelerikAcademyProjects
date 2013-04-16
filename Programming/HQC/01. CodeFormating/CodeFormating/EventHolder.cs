d3d71b395aa5fca2433fd18f033b240e6e0fd973
zdravko <zddavko@gmail.com>
2013-04-16 10:20:54 +0300

    fixed

---
+++ Programming/HQC/01. CodeFormating/CodeFormating/EventHolder.cs
@@ -0,0 +1,56 @@
+﻿namespace CodeFormating
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    internal class EventHolder
+    {
+        private MultiDictionary<string, Event> title = new MultiDictionary<string, Event>(true);
+        private OrderedBag<Event> date = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+            this.title.Add(title.ToLower(), newEvent);
+            this.date.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in this.title[title])
+            {
+                removed++;
+                this.date.Remove(eventToRemove);
+            }
+
+            this.title.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.date.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
---
+++ Programming/HQC/02. NameIdentifiers/RefactorTheCode/Converter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Converter
+{
+    public void ConvertBoolToString(bool input)
+    {
+        string inputToString = input.ToString();
+        Console.WriteLine(inputToString);
+    }
+}
