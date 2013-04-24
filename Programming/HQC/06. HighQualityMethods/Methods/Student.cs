47ddbe2a2dca9fc056e8b5cb589e29288e72b38f
zdravko <zddavko@gmail.com>
2013-04-24 12:46:45 +0300

    fixed

---
+++ Programming/HQC/06. HighQualityMethods/Methods/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Country { get; private set; }
+        public byte Age { get; private set; }
+
+        public Student(string firstName, string lastName, byte age, string country)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public bool IsOlderThan(Student other)
+        {
+            if (this.Age > other.Age)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
