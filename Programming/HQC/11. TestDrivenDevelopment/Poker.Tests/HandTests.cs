6e6a6f77f0ec45309b9cc749364242f9809c4472
zdravko <zddavko@gmail.com>
2013-05-03 16:55:05 +0300

    fixed

---
+++ Programming/HQC/11. TestDrivenDevelopment/Poker.Tests/HandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poker.Tests
+{
+    [TestClass]
+    public class HandTests
+    {
+        [TestMethod]
+        public void TestHandHaveSpecificCards()
+        {
+            Hand hand = new Hand(
+                new Card(CardFace.Ace, CardSuit.Spades),
+                new Card(CardFace.Jack, CardSuit.Diamonds),
+                new Card(CardFace.Three, CardSuit.Spades),
+                new Card(CardFace.Five, CardSuit.Hearts),
+                new Card(CardFace.Two, CardSuit.Clubs)
+                );
+
+            string actual = hand.ToString();
+            string expected = "A♠J♦3♠5♥2♣";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
