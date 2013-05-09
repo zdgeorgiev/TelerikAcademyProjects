cd1331d68852f4848abf9bf408b20e8338d6fcff
zdravko <zddavko@gmail.com>
2013-05-09 20:42:04 +0300

    fixed

---
+++ Programming/HQC/12. Refactoring/RotatingMatrix.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+public class RotatingMatrix
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a positive number: ");
+        string input = Console.ReadLine();
+        int n = 0;
+        
+        while (!int.TryParse(input, out n))
+        {
+            input = Console.ReadLine();
+        }
+
+        int[,] matrix = GenerateRotationWalkMatrix(n);
+
+        PrintOnConsole(matrix);
+    }
+
+    public static int[,] GenerateRotationWalkMatrix(int n)
+    {
+        int[,] matrix = new int[n, n];
+        int nextNumber = 1;
+        int currectRow = 0;
+        int currectCol = 0;
+        int directionRow = 1;
+        int directionCol = 1;
+
+        while (true)
+        {
+            matrix[currectRow, currectCol] = nextNumber;
+
+            if (!CheckForAvaibleCell(matrix, currectRow, currectCol))
+            {
+                FindCell(matrix, out currectRow, out currectCol);
+
+                nextNumber++;
+
+                if (currectRow != 0 && currectCol != 0)
+                {
+                    directionRow = 1; directionCol = 1;
+
+                    while (true)
+                    {
+                        matrix[currectRow, currectCol] = nextNumber;
+
+                        if (!CheckForAvaibleCell(matrix, currectRow, currectCol))
+                        {
+                            break;
+                        }
+
+                        while (NextDistinationIsntAvaible(matrix, currectRow, currectCol, directionRow, directionCol))
+                        {
+                            RotateUntilFindAvaibleDestination(ref directionRow, ref directionCol);
+                        }
+
+                        currectRow += directionRow;
+                        currectCol += directionCol;
+                        nextNumber++;
+                    }
+                }
+
+                break;
+            }
+
+            while (NextDistinationIsntAvaible(matrix, currectRow, currectCol, directionRow, directionCol))
+            {
+                RotateUntilFindAvaibleDestination(ref directionRow, ref directionCol);
+            }
+
+            currectRow += directionRow;
+            currectCol += directionCol;
+            nextNumber++;
+        }
+
+        return matrix;
+    }
+
+    private static bool NextDistinationIsntAvaible(int[,] matrix, int currectRow, int currectCol, int directionRow, int directionCol)
+    {
+        return CheckForInvalidRow(matrix, currectRow, directionRow) ||
+               CheckForInvalidCol(matrix, currectCol, directionCol) ||
+               CheckIfNextCellIsNotAvaible(matrix, currectRow, currectCol, directionRow, directionCol);
+    }
+
+    private static bool CheckIfNextCellIsNotAvaible(int[,] matrix, int currectRow, int currectCol, int directionRow, int directionCol)
+    {
+        return matrix[currectRow + directionRow, currectCol + directionCol] != 0;
+    }
+
+    private static bool CheckForInvalidRow(int[,] matrix, int currectRow, int directionRow)
+    {
+        return currectRow + directionRow >= matrix.GetLength(0) || currectRow + directionRow < 0;
+    }
+
+    private static bool CheckForInvalidCol(int[,] matrix, int currectCol, int directionCol)
+    {
+        return currectCol + directionCol >= matrix.GetLength(1) || currectCol + directionCol < 0;
+    }
+
+    static bool CheckForAvaibleCell(int[,] arr, int row, int col)
+    {
+        int[] dirRow = { 1, 1, 1, 0, -1, -1, -1, 0 };
+        int[] dirCol = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+        for (int i = 0; i < dirRow.Length; i++)
+        {
+            if (row + dirRow[i] >= arr.GetLength(0) || row + dirRow[i] < 0)
+            {
+                dirRow[i] = 0;
+            }
+
+            if (col + dirCol[i] >= arr.GetLength(0) || col + dirCol[i] < 0)
+            {
+                dirCol[i] = 0;
+            }
+        }
+
+        for (int i = 0; i < dirRow.Length; i++)
+        {
+            if (arr[row + dirRow[i], col + dirCol[i]] == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void RotateUntilFindAvaibleDestination(ref int dx, ref int dy)
+    {
+        int[] dirRow = { 1, 1, 1, 0, -1, -1, -1, 0 };
+        int[] dirCol = { 1, 0, -1, -1, -1, 0, 1, 1 };
+        int cd = 0;
+
+        for (int count = 0; count < dirRow.Length; count++)
+        {
+            if (dirRow[count] == dx && dirCol[count] == dy)
+            {
+                cd = count;
+                break;
+            }
+        }
+
+        if (cd == 7)
+        {
+            dx = dirRow[0];
+            dy = dirCol[0];
+            return;
+        }
+
+        dx = dirRow[cd + 1];
+        dy = dirCol[cd + 1];
+    }
+
+    static void FindCell(int[,] arr, out int row, out int col)
+    {
+        row = 0;
+        col = 0;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] == 0)
+                {
+                    row = i;
+                    col = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    static void PrintOnConsole(int[,] matix)
+    {
+        for (int row = 0; row < matix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matix.GetLength(1); col++)
+            {
+                Console.Write("{0,4}", matix[row, col]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programming/HQC/12. Refactoring/WalkInMatrixTests/RotatingMatrixTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace MatrixTests
+{
+    [TestClass]
+    public class RotatingMatrixTest
+    {
+        [TestMethod]
+        public void TestOutputMatrixWith5()
+        {
+            int n = 5;
+            int[,] expectedMatrix =
+            {
+                { 1, 13, 14, 15, 16},
+                {12,  2, 21, 22, 17},
+                {11, 23,  3, 20, 18},
+                {10, 25, 24,  4, 19},
+                { 9,  8,  7,  6,  5}
+            };
+
+            string expected = MatrixAsString(expectedMatrix);
+            int[,] actualMatrix = RotatingMatrix.GenerateRotationWalkMatrix(n);
+            string actual = MatrixAsString(actualMatrix);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestOutputMatrixWith4()
+        {
+            int n = 4;
+            int[,] expectedMatrix =
+            {
+                {1, 10, 11, 12},
+                {9,  2, 15, 13},
+                {8, 16,  3, 14},
+                {7,  6,  5,  4}
+            };
+
+            string expected = MatrixAsString(expectedMatrix);
+            int[,] actualMatrix = RotatingMatrix.GenerateRotationWalkMatrix(n);
+            string actual = MatrixAsString(actualMatrix);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestOutputMatrixWith7()
+        {
+            int n = 7;
+            int[,] expectedMatrix =
+            {
+                { 1, 19, 20, 21, 22, 23, 24},
+                {18,  2, 33, 34, 35, 36, 25},
+                {17, 40,  3, 32, 39, 37, 26},
+                {16, 48, 41,  4, 31, 38, 27},
+                {15, 47, 49, 42,  5, 30, 28},
+                {14, 46, 45, 44, 43,  6, 29},
+                {13, 12, 11, 10,  9,  8,  7}
+            };
+
+            string expected = MatrixAsString(expectedMatrix);
+            int[,] actualMatrix = RotatingMatrix.GenerateRotationWalkMatrix(n);
+            string actual = MatrixAsString(actualMatrix);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestInvalidOutputMatrixWith7()
+        {
+            int n = 7;
+            int[,] expectedMatrix =
+            {
+                { 1, 19, 20, 21, 22, 23, 24},
+                {18,  2, 33, 34, 35, 36, 25},
+                {17, 39,  3, 32, 39, 37, 26},
+                {16, 48, 41,  4, 31, 38, 27},
+                {15, 47, 49, 42,  5, 30, 28},
+                {14, 46, 45, 44, 43,  6, 29},
+                {13, 12, 11, 10,  9,  8,  7}
+            };
+
+            string expected = MatrixAsString(expectedMatrix);
+            int[,] actualMatrix = RotatingMatrix.GenerateRotationWalkMatrix(n);
+            string actual = MatrixAsString(actualMatrix);
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestInvalidOutputMatrixWith7InFirstPosition()
+        {
+            int n = 7;
+            int[,] expectedMatrix =
+            {
+                {50, 19, 20, 21, 22, 23, 24},
+                {18,  2, 33, 34, 35, 36, 25},
+                {17, 40,  3, 32, 39, 37, 26},
+                {16, 48, 41,  4, 31, 38, 27},
+                {15, 47, 49, 42,  5, 30, 28},
+                {14, 46, 45, 44, 43,  6, 29},
+                {13, 12, 11, 10,  9,  8,  7}
+            };
+
+            string expected = MatrixAsString(expectedMatrix);
+            int[,] actualMatrix = RotatingMatrix.GenerateRotationWalkMatrix(n);
+            string actual = MatrixAsString(actualMatrix);
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        public string MatrixAsString(int[,] matrix)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    result.Append(matrix[i, j] + " ");
+                }
+            }
+
+            result.Length--;
+            return result.ToString();
+        }
+    }
+}
