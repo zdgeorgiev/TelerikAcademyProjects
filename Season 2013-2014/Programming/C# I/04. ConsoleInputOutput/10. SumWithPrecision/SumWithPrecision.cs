aba84e4a1286d5ffaa6fa98c6596324b0e86647e
zdravko <zddavko@gmail.com>
2013-05-24 14:17:56 +0300

    fixed

---
+++ Season 2013-2014/Programming/C# I/04. ConsoleInputOutput/10. SumWithPrecision/SumWithPrecision.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class SumWithPrecision
+{
+    static void Main()
+    {
+        decimal sum = 1;
+        decimal oldSum = -1;
+
+        for (decimal i = 2; i <= 9950; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum = sum + (1 / i);
+            }
+            else
+            {
+                sum = sum - (1 / i);
+            }
+
+            if ((sum - oldSum) == 0.001M)
+            {
+                break;
+            }
+            else
+            {
+                oldSum = sum;
+            }
+
+        }
+
+        Console.WriteLine(sum);
+    }
+}
