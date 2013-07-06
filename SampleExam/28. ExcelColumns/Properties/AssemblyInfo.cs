1dd0a6d479be04d1ad4d6e36b82858279d92727e
zdravko <zddavko@gmail.com>
2013-07-06 16:39:40 +0300

    fixed

--- SampleExam/28. ExcelColumns/Properties/AssemblyInfo.cs
+++ SampleExam/28. ExcelColumns/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("28.ExcelColumns")]
+[assembly: AssemblyTitle("28. ExcelColumns")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("28.ExcelColumns")]
+[assembly: AssemblyProduct("28. ExcelColumns")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ca3494be-ecce-4407-9178-88794845a8eb")]
+[assembly: Guid("6bf5b6fb-b7e0-497f-948b-ed60ee439525")]
 
 // Version information for an assembly consists of the following four values:
 //
