5eb9158900ffdacbf020c0984eceed43825431f0
zdravko <zddavko@gmail.com>
2013-07-29 02:22:10 +0300

    fixed

---
+++ Season 2013-2014/Programming/C# II/08. Strings and Text Processing/24. HTMLValues/HTMLValues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class HTMLValues
+{
+    static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../html.txt");
+
+        string pattern = @"(?<=^|>)[^><]+?(?=<|$)";
+
+        using (reader)
+        {
+            MatchCollection values = Regex.Matches(reader.ReadToEnd(), pattern);
+
+            foreach (var value in values)
+            {
+                if (!String.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    Console.WriteLine(value); 
+                }
+            }
+        }
+    }
+}
+
+//Write a program that extracts from given HTML
+//file its title (if available), and its body text without the HTML tags.
