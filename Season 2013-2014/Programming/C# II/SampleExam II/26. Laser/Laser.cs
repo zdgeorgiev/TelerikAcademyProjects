279cffc9b1f132a8458b2a026fde2763710614b5
zdravko <zddavko@gmail.com>
2013-09-12 01:13:55 +0300

    fixed

--- Season 2013-2014/Programming/C# II/SampleExam II/26. Laser/Laser.cs
+++ Season 2013-2014/Programming/C# II/SampleExam II/26. Laser/Laser.cs
@@ -1,63 +1,83 @@
 ﻿using System;
-
+ 
 class Laser
 {
     private static int width, height, depth;
     private static int laserWidth, laserHeight, laserDepth;
-
+ 
     static void Main(string[] args)
     {
         string[] cubeDimentions = Console.ReadLine().Split();
-
+ 
         width = int.Parse(cubeDimentions[0]);
         height = int.Parse(cubeDimentions[1]);
         depth = int.Parse(cubeDimentions[2]);
-
+ 
         int[, ,] cube = new int[width + 1, height + 1, depth + 1];
-
+ 
         string[] laserCoords = Console.ReadLine().Split();
-
+ 
         laserWidth = int.Parse(laserCoords[0]);
         laserHeight = int.Parse(laserCoords[1]);
         laserDepth = int.Parse(laserCoords[2]);
-
+ 
         string[] laserDirection = Console.ReadLine().Split();
-
+ 
         int laserDirWidth = int.Parse(laserDirection[0]);
         int laserDirHeight = int.Parse(laserDirection[1]);
         int laserDirDepth = int.Parse(laserDirection[2]);
-
+ 
         int nextWidth = laserWidth;
         int nextHeight = laserHeight;
         int nextDepth = laserDepth;
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> cdd8653caf76bfa1b23bb523e03f1c174d99b1e4
         while (true)
         {
             cube[nextWidth, nextHeight, nextDepth] = -1;
             int count = 0;
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> cdd8653caf76bfa1b23bb523e03f1c174d99b1e4
             if (nextWidth + laserDirWidth < 1 || nextWidth + laserDirWidth > width)
             {
                 ChangeTheDirection(ref laserDirWidth);
                 count++;
             }
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> cdd8653caf76bfa1b23bb523e03f1c174d99b1e4
             if (nextHeight + laserDirHeight < 1 || nextHeight + laserDirHeight > height)
             {
                 ChangeTheDirection(ref laserDirHeight);
                 count++;
             }
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> cdd8653caf76bfa1b23bb523e03f1c174d99b1e4
             if (nextDepth + laserDirDepth < 1 || nextDepth + laserDirDepth > depth)
             {
                 ChangeTheDirection(ref laserDirDepth);
                 count++;
             }
-
+ 
             nextWidth += laserDirWidth;
             nextHeight += laserDirHeight;
             nextDepth += laserDirDepth;
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> cdd8653caf76bfa1b23bb523e03f1c174d99b1e4
             if (cube[nextWidth, nextHeight, nextDepth] == -1 || count == 3)
             {
                 Console.WriteLine("{0} {1} {2}",
@@ -66,7 +86,7 @@ class Laser
             }
         }
     }
-
+ 
     private static void ChangeTheDirection(ref int direction)
     {
         if (direction == 1)
