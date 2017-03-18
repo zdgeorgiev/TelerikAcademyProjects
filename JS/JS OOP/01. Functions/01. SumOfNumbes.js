d146a6024e75af57553013314b70433be9789e9b
zdgeorgiev <zddavko@gmail.com>
2017-03-18 23:55:29 +0200

    fixed

---
+++ JS/JS OOP/01. Functions/01. SumOfNumbes.js
@@ -0,0 +1,31 @@
+/* Task Description */
+/* 
+	Write a function that sums an array of numbers:
+		numbers must be always of type Number
+		returns `null` if the array is empty
+		throws Error if the parameter is not passed (undefined)
+		throws if any of the elements is Number	
+*/
+function solve() {
+
+    if (typeof arguments === 'undefined' || typeof arguments[0] !== 'object')
+        throw Error("First argument must be an array");
+
+    const arr = arguments[0];
+
+    if (arr.length === 0)
+        return null;
+
+    return (function sum() {
+        return arr
+            .filter(x => {
+                if (typeof x !== 'number') {
+                    throw Error(`All parameters must be numbers: ${x}`);
+                }
+                return x;
+            })
+            .reduce((x, y) => x + y);
+    })();
+}
+
+module.exports = solve;
---
+++ JS/JS OOP/01. Functions/02. PrimeNumbersInRange.js
@@ -0,0 +1,41 @@
+/* Task Description */
+/*
+	Write a function that finds all the prime numbers in a range
+		1) it should return the prime numbers in an array
+		2) it must throw an Error if any on the range params is not convertible to `Number`
+		3) it must throw an Error if any of the range params is missing
+*/
+function solve(start, end) {
+
+    if (typeof start === 'undefined' || Number(start).toString() === `NaN` ||
+        typeof end === 'undefined' || Number(end).toString() === `NaN`) {
+        throw Error('Start and end parameters must be convertable to numbers');
+    }
+
+    return (function findPrimes() {
+        let primes = [],
+            isPrime = true;
+
+        for (let i = start; i <= end; i++) {
+
+            isPrime = true
+
+            // Slow O(N^2)
+            for (let j = 2; j < i; j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime && i !== 1)
+                primes.push(i);
+        }
+
+        return primes;
+    })();
+}
+
+module.exports = solve;
+
+console.log(solve(1, 10));
